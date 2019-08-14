5676a746c3832db6d733ce6c33a74bfeac689449
shawn-collette <shawn.collette@gmail.com>
2019-08-14 11:21:40 -0600

    4 assignments

---
+++ iteratingThroughObjects.js
@@ -0,0 +1,23 @@
+/* eslint-disable strict */
+
+/**
+ *  Iterating Through Objects 8/14/2019
+ */
+
+//Assignment 1
+//Make student reports
+
+
+
+//Assignment 2
+//Enroll in summer school
+
+
+
+//Assignment 3
+//Find by ID
+
+
+
+//Assignemtn 4
+//Validate object keys
